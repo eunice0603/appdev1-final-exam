9e3aab6d70b12170d16109c18dbdf0d14fa39bc7
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-11-29 11:11:20 +0800

    Update readme.md and add live url

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Todos from "./pages/Todos"
 
 function App () {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/todos" element={<Todos />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/todos" element={<Todos />} />
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import "./App.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
